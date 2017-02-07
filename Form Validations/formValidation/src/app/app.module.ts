e1704a47af9ce40244aecfce90a78fb3b6596c8b
Piyush Mittal <m@piyushmittal.com>
2017-02-07 13:56:32 +0530

    Form validations
    A simple form(template form), Custom form, and validations on this
    custom form.

--- Form Validations/formValidation/src/app/app.module.ts
+++ Form Validations/formValidation/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+// import the 3 components we created.
+import { SimpleformComponent } from './simpleform/simpleform.component';
+import { ComplexformComponent } from './complexform/complexform.component';
+import { FormvalidationsComponent } from './formvalidations/formvalidations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // declare the 3 components as part of root @NgModule
+    SimpleformComponent,
+    ComplexformComponent,
+    FormvalidationsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    // Include reactive forms in our app in @NgModule imports block
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Form Validations/formValidation/src/app/simpleform/simpleform.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simpleform',
+  templateUrl: './simpleform.component.html',
+  styleUrls: ['./simpleform.component.css']
+})
+export class SimpleformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+	// Here we are implementing the submitForm function. All we are doing for right now is spitting out the details of the form to our console.
+  submitForm1(form: any): void{
+    console.log('Form Data: ');
+    console.log(form);
+  }
+}
